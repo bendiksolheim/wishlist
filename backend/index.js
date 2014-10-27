89eb4e5047deaddd34cc52ff3b563923f93a22fc
Bendik Solheim <bendik@solheim.im>
2014-10-27 19:11:20 +0100

    Added some logging

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var restful = require('node-restful');
 var bodyParser = require('body-parser');
+var log = require('util').log;
 var mongoose = restful.mongoose;
 
 var app = express();
@@ -8,7 +9,18 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.query());
 
+app.use(function(req, res, next) {
+	log(req.method + '\t' + req.protocol +  '://' + req.get('host') + req.originalUrl);
+	next();
+});
+
 mongoose.connect('mongodb://localhost/wishlist');
+mongoose.connection.on('connected', function() {
+	log('Connected to mongodb');
+});
+mongoose.connection.on('error', function(err) {
+	log('Error connecting to mongodb: ', err);
+});
 
 var wish = app.wish = restful.model('wishes', mongoose.Schema({
 	name: {type: String, required: true},
